4cbd9c62f1c487185987feb1b3b14b801e4d5bbf
kb <86767129+kbdevs@users.noreply.github.com>
2025-04-25 16:31:04 -0700

    Update kindlefetch.sh

--- kindlefetch/bin/kindlefetch.sh
+++ kindlefetch/bin/kindlefetch.sh
@@ -45,7 +45,7 @@ main_menu() {
 | . \| | | | | (_| | |  __/ | |  __/ || (__| | | |
 |_|\_\_|_| |_|\__,_|_|\___|_|  \___|\__\___|_| |_|
                                                 
-$(load_version) | https://github.com/justrals/KindleFetch
+$(load_version) | https://github.com/kbdevs/KindleFetch
 "
         if $UPDATE_AVAILABLE; then
             echo "Update available! Select option 6 to install."
